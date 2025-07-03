599fe22fa3950ecf7e9928fb37f27525a8ef3712
Darina202 <138057834+Darina202@users.noreply.github.com>
2025-07-03 18:25:45 +0300

    create form and list

---
+++ src/components/Stats.js
@@ -0,0 +1,10 @@
+export default function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        You got everything! Ready to go ✈️ 💼 You have items on your list, and
+        you already packed
+      </em>
+    </footer>
+  );
+}
