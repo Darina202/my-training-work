6efa4a3230cd3b15c17d44a68561c28ffe0f5f53
Darina202 <138057834+Darina202@users.noreply.github.com>
2025-07-30 22:45:54 +0300

    create starRating

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import StarRating from "./components/usepopcorn/StarRating/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <StarRating maxRating={5} />
+    <StarRating size={24} color="blue" defaultRating={1} />
   </React.StrictMode>
 );
